2731099da438c30e21cd6437ca307d880d20ffe9
willianchu <willianchu@hotmail.com>
2022-05-02 19:55:36 -0700

    add a function index in views with tasks as argument

--- app3in1/tasks/views.py
+++ app3in1/tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+tasks = ["Étudier Français", "Étudier l'anglais", "Étudier AWS", "Étudier Python" ]
+
 # Create your views here.
+def index(request):
+    return render(request, 'tasks/index.html', {
+      'tasks': tasks
+    })
