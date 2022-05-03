29a47bab74590e1a7d9f267cb3d8dcc408cdd622
willianchu <willianchu@hotmail.com>
2022-05-02 21:26:28 -0700

    update a post response in function with a reverse path to main page

--- app3in1/tasks/views.py
+++ app3in1/tasks/views.py
@@ -1,11 +1,13 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 tasks = ["Étudier Français", "Étudier l'anglais", "Étudier AWS", "Étudier Python" ]
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label='Nouvelle tâche')
-    priority = forms.IntegerField(label='Priorité', min_value=1, max_value=5)
+    # priority = forms.IntegerField(label='Priorité', min_value=1, max_value=5)
 
 # Create your views here.
 def index(request):
@@ -14,7 +16,17 @@ def index(request):
     })
 
 def add(request):
-    # tasks.append(request.POST['task'])
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            tasks.append(task)
+            return HttpResponseRedirect(reverse('tasks:index'))
+        else:
+          return render(request, 'tasks/add.html', {
+            'form': form
+          })
+
     return render(request, 'tasks/add.html',{
       'form': NewTaskForm()
     })
