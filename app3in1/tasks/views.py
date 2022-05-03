7616a46270a4cbc6631f6b0fe3dc64405c665fd1
willianchu <willianchu@hotmail.com>
2022-05-02 21:08:02 -0700

    add a form validation by automated django library

--- app3in1/tasks/views.py
+++ app3in1/tasks/views.py
@@ -1,6 +1,11 @@
+from django import forms
 from django.shortcuts import render
 
 tasks = ["Étudier Français", "Étudier l'anglais", "Étudier AWS", "Étudier Python" ]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label='Nouvelle tâche')
+    priority = forms.IntegerField(label='Priorité', min_value=1, max_value=5)
 
 # Create your views here.
 def index(request):
@@ -10,4 +15,6 @@ def index(request):
 
 def add(request):
     # tasks.append(request.POST['task'])
-    return render(request, 'tasks/add.html')
+    return render(request, 'tasks/add.html',{
+      'form': NewTaskForm()
+    })
